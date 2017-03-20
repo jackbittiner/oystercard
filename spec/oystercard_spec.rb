d77f1e193b196e4caefc7ad7a4b86815365c33f7
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-20 17:53:57 +0000

    Refactored top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,8 +11,7 @@ describe Oystercard do
 
   describe '#top_up' do
     it "adds money to the card" do
-      subject.top_up(10)
-      expect(subject.balance).to eq 10
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
 
 end
