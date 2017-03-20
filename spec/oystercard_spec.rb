01160a65c4db35b0da53c3262b90d915b20b8018
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-20 17:50:13 +0000

    create test to top up card and give it a new balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
+  it { is_expected.to respond_to(:top_up).with(1).argument}
+
   describe '#balance' do
     it "shows balance on the card" do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#top_up' do
+    it "adds money to the card" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+end
 
 end
