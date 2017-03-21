0079dbcfb556f58fe2878730414f69e76f1a4bb1
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-21 10:04:08 +0000

    Added test for maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,14 +4,21 @@ describe Oystercard do
   it { is_expected.to respond_to(:top_up).with(1).argument}
 
   describe '#balance' do
+
     it "shows balance on the card" do
       expect(subject.balance).to eq 0
     end
   end
 
   describe '#top_up' do
+
     it "adds money to the card" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'raises an error when the balance surpasses 90' do
+      subject.top_up 90
+      expect {subject.top_up 1}.to raise_error "Maximum Balance Reached"
     end
 
 end
