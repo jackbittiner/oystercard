9326020c26c3e6f686f196a9bf2209fe6da2e5d8
Freddy Fallon <freddyfallon@gmail.com>
2017-03-21 10:24:35 +0000

    Add test to check for deduct method with 1 argument

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -22,6 +22,9 @@ describe Oystercard do
       expect {subject.top_up 1}.to raise_error "Maximum Balance of #{maximum_balance} Reached"
     end
 
-end
+  end
 
+  describe '#deduct' do
+      it { is_expected.to respond_to(:deduct).with(1).argument }
+  end
 end
